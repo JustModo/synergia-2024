11ede7510c6251a82ee39ad20d0814fb7e0bc288
Vivek Neer <139262048+VivekNeer@users.noreply.github.com>
2024-09-06 12:24:38 +0530

    anim front page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Logo from "./components/Logo";
 import TitleHeader from "./components/TitleHeader";
 import Scroller from "./components/Scroller";
 import Countdown from "./components/Countdown";
+import LargeHeader from "./components/LargeHeader";
 
 export default function App() {
   const containerRef = useRef(null);
@@ -21,8 +22,10 @@ export default function App() {
         className="h-screen relative flex justify-center items-center -z-50 overflow-hidden"
         ref={containerRef}
       >
+        <LargeHeader />
         <Logo containerRef={containerRef} />
         <Countdown />
+        <div className="absolute bottom-0 h-56 w-full bg-gradient-to-t from-black to-transparent"/>
         {/* <div className="absolute bottom-0 w-full h-28 bg-gradient-to-t from-black to-transparent"></div> */}
       </motion.div>
       <motion.div className="w-full md:px-14 lg:px-44 px-4 py-16 pt-32 min-h-screen">
